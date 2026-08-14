06c84a3907535b2e9a4b6088cef30d94a93f7c78
Brent Bolton <brent.bolton@cfacorp.com>
2026-08-14 12:40:16 -0400

    Add missing useRef import in ConnectionStarfieldOverlay.tsx

--- src/components/ConnectionStarfieldOverlay.tsx
+++ src/components/ConnectionStarfieldOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 export type ConnectPhase = "idle" | "connecting" | "succeeded" | "failed";
 
